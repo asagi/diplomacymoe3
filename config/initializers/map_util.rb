d3054f9336bacdb39164e4fda4c4e2c000980bb0
Keiju Asagi <k.asagi@nfity.com>
2020-02-03 10:08:47 +0900

    style: Cleanup codes

--- config/initializers/map_util.rb
+++ config/initializers/map_util.rb
@@ -17,10 +17,10 @@ class MapUtil < Settingslogic
   end
 
   def self.distance(start:, to:)
-    return 0 if to.include?(start)
+    return 0 if to == start
 
     distances = { start => 0 }
-    calc_distance(current: start, to: to, distances: distances)
+    distances = calc_distance(current: start, to: to, distances: distances)
     distances[to]
   end
 
@@ -31,14 +31,14 @@ class MapUtil < Settingslogic
   end
 
   def self.update_shortest_distance_from_start(current:, distances:)
-    adjacencies[current].each do |prov_code, _data|
+    adjacencies[current].each_key do |prov_code|
       distances[prov_code] ||= max_provinces
       distances[prov_code] = [distances[prov_code], distances[current] + 1].min
     end
     distances
   end
 
-  def self.farther_adjacencies_of_start(current:, distances:)
+  def self.adjacencies_farther_from_start(current:, distances:)
     distances = update_shortest_distance_from_start(
       current: current,
       distances: distances
@@ -49,13 +49,13 @@ class MapUtil < Settingslogic
   end
 
   def self.calc_distance(current:, to:, distances:, depth: 0)
-    return if (distances[to] || max_provinces) < depth
+    return distances if (distances[to] || max_provinces) < depth
 
-    farther_adjacencies_of_current = farther_adjacencies_of_start(
+    adjacencies = adjacencies_farther_from_start(
       current: current,
       distances: distances
     )
-    farther_adjacencies_of_current.each do |prov_code|
+    adjacencies.each do |prov_code|
       calc_distance(
         current: prov_code,
         to: to,
@@ -63,5 +63,6 @@ class MapUtil < Settingslogic
         depth: depth + 1
       )
     end
+    distances
   end
 end
