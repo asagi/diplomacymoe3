24386d57c2b7e4dc7ae181fd2a52a2f47d19df1e
Keiju Asagi <asagi@tepidworks.jp>
2020-02-03 23:17:28 +0900

    test: Add table creation request test

--- spec/requests/tables_spec.rb
+++ spec/requests/tables_spec.rb
@@ -57,6 +57,77 @@ RSpec.describe 'tables', type: :request do
     end
   end
 
+  describe 'POST /api/tables' do
+    context '旗/変動/短期/2020-01-09/1:00/鍵あり/英' do
+      before :example do
+        params_json = <<~'JSON'
+          {
+            "face_type": "flag",
+            "period_rule": "flexible",
+            "duration": "short",
+            "juggling": "disallow",
+            "private": true,
+            "keyword": "aaa",
+            "due_date": "2020-01-09",
+            "start_time": "1:00",
+            "desired_power": "e"
+          }
+        JSON
+        travel_to('2020-01-08 06:50') do
+          post tables_path, params: params_json, headers: headers
+        end
+      end
+
+      example 'API が正常に終了する' do
+        expect(response).to be_successful
+      end
+
+      example '201 が返ってくる' do
+        expect(response.status).to eq 201
+      end
+
+      example 'レスポンスに ID が含まれている' do
+        json = JSON.parse(response.body)
+        expect(json['id']).to be_positive
+      end
+
+      example '生成された卓の face_type が旗' do
+        json = JSON.parse(response.body)
+        expect(Table.find(json['id']).regulation.face_type).to eq 'flag'
+      end
+
+      example '生成された卓の period_rule が変動' do
+        json = JSON.parse(response.body)
+        expect(Table.find(json['id']).regulation.period_rule).to eq 'flexible'
+      end
+
+      example '生成された卓の duration が短期' do
+        json = JSON.parse(response.body)
+        expect(Table.find(json['id']).regulation.duration).to eq 'short'
+      end
+
+      example '生成された卓の juggling が不許可' do
+        json = JSON.parse(response.body)
+        expect(Table.find(json['id']).regulation.juggling).to eq 'disallow'
+      end
+
+      example '生成された卓の private が true' do
+        json = JSON.parse(response.body)
+        expect(Table.find(json['id']).regulation.private).to be true
+      end
+
+      example '生成された卓の keyword が aaa' do
+        json = JSON.parse(response.body)
+        expect(Table.find(json['id']).regulation.keyword).to eq 'aaa'
+      end
+
+      example '生成された卓の卓主の希望国が英' do
+        json = JSON.parse(response.body)
+        expect(Table.find(json['id']).players.first.desired_power).to eq 'e'
+      end
+    end
+  end
+
   describe 'GET /api/tables' do
     context '1 件もない場合' do
       before :example do
